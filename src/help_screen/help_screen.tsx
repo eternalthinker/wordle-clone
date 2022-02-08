5bc7c167dc8401bfa106874024750b73c72bf52a
Rahul Anand <rahul@canva.com>
2022-02-08 12:41:15 +1100

    fixes

--- src/help_screen/help_screen.tsx
+++ src/help_screen/help_screen.tsx
@@ -11,21 +11,37 @@ export const HelpScreen = ({ onCloseClick }: { onCloseClick: () => void }) => {
       </div>
       <p>
         This is a clone of the popular{" "}
-        <a href="https://www.powerlanguage.co.uk/wordle/">Wordle game.</a>
+        <a
+          href="https://www.powerlanguage.co.uk/wordle/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Wordle game.
+        </a>
       </p>
+      <p>This version also gives you the option to play with 6-letter words.</p>
       <p>
-        This version also gives you the option to play with 6-letter words.
-      </p>
-      <p>
-        The 5-letter mode uses the same daily solutions as the original Wordle game.
+        The 5-letter mode uses the same daily solutions as the original Wordle
+        game.
       </p>
       <h3>Links</h3>
-      Made by <a href="https://github.com/eternalthinker">
+      Made by{" "}
+      <a
+        href="https://github.com/eternalthinker"
+        target="_blank"
+        rel="noreferrer"
+      >
         eternalthinker
       </a>{" "}
       <br />
       Wordle solver:{" "}
-      <a href="https://github.com/eternalthinker/wordle-helper">Solver</a>
+      <a
+        href="https://github.com/eternalthinker/wordle-helper"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Solver
+      </a>
     </div>
   );
 };
